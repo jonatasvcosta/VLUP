b0f539dbbb1da49a64519d173fe9b6338707436b
jonatasvcosta <jonatas.atsoc@gmail.com>
2016-11-12 00:30:09 -0200

    Added LoginActivity And Layout

---
+++ app/src/main/java/politcc2017/tcc_app/Activities/LoginActivity.java
@@ -0,0 +1,28 @@
+package politcc2017.tcc_app.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+import politcc2017.tcc_app.R;
+
+/**
+ * Created by Jonatas on 25/10/2016.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_maintenance);
+        setActivityTitle("Maintenance Activity");
+        Button mButton = (Button) findViewById(R.id.maintenance_back_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startOrResumeActivity(HomeActivity.class, true);
+            }
+        });
+    }
+}
