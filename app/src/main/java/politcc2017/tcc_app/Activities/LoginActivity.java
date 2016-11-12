29f835c3f283040292f649fcc57bf2180c35f1a5
jonatasvcosta <jonatas.atsoc@gmail.com>
2016-11-12 00:30:20 -0200

    Added LoginActivity And Layout

--- app/src/main/java/politcc2017/tcc_app/Activities/LoginActivity.java
+++ app/src/main/java/politcc2017/tcc_app/Activities/LoginActivity.java
@@ -1,28 +1,42 @@
 package politcc2017.tcc_app.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
+import politcc2017.tcc_app.Components.CustomButton;
 import politcc2017.tcc_app.R;
 
 /**
  * Created by Jonatas on 25/10/2016.
  */
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_maintenance);
-        setActivityTitle("Maintenance Activity");
-        Button mButton = (Button) findViewById(R.id.maintenance_back_button);
-        mButton.setOnClickListener(new View.OnClickListener() {
+        super.setContentView(R.layout.activity_login);
+        ActionBar bar = getSupportActionBar();
+        if(bar != null) bar.hide();
+        CustomButton loginButton = (CustomButton) findViewById(R.id.login_activity_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startOrResumeActivity(HomeActivity.class, true);
+            public void onClick(View view) {
+                if(validateLogin()) startHomeActivity();
             }
         });
+    }
+
+    protected boolean validateLogin(){
+        return true;
+    }
+
+    protected void startHomeActivity(){
+        Intent intent = new Intent(getBaseContext(), HomeActivity.class);
+        finishAffinity();
+        startActivity(intent);
     }
 }
--- app/src/main/java/politcc2017/tcc_app/Activities/MaintenanceActivity.java
+++ app/src/main/java/politcc2017/tcc_app/Activities/MaintenanceActivity.java
@@ -1,11 +1,9 @@
 package politcc2017.tcc_app.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import politcc2017.tcc_app.R;
 
