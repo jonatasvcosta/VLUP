bfb460dd93049e8823e8dd74e2be6895dcbd3678
jonatasvcosta <jonatas.atsoc@gmail.com>
2016-11-29 00:06:53 -0200

    Added clipboard change listener

--- app/src/main/java/politcc2017/tcc_app/Activities/NavigateActivity.java
+++ app/src/main/java/politcc2017/tcc_app/Activities/NavigateActivity.java
@@ -1,10 +1,15 @@
 package politcc2017.tcc_app.Activities;
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ContextMenu;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import politcc2017.tcc_app.R;
 
@@ -30,5 +35,31 @@ public class NavigateActivity extends BaseActivity {
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
         mWebView.setWebViewClient(new WebViewClient());
+        registerForContextMenu(mWebView);
+        setupClipboardListener();
     }
+
+    private void setupClipboardListener(){
+        final ClipboardManager clipboard = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+
+        clipboard.addPrimaryClipChangedListener( new ClipboardManager.OnPrimaryClipChangedListener() {
+            public void onPrimaryClipChanged() {
+                String a = clipboard.getText().toString();
+                Toast.makeText(getBaseContext(),a,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        //user has long pressed your TextView
+
+        //menu.add(0, v.getId(), 0, "text that you want to show in the context menu - I use simply Copy");
+        //cast the received View to TextView so that you can get its text
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        //Toast.makeText(getBaseContext(), clipboard.getText().toString(), Toast.LENGTH_SHORT).show();
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
 }
