639a71cf1bcb329b79c01500357dcac61f8a372d
Rogério Motisuki <rogerioyuuki@gmail.com>
2017-02-08 19:03:02 -0200

    Added dev database image

--- APP-SERVER/dev-run.sh
+++ APP-SERVER/dev-run.sh
@@ -10,14 +10,25 @@ docker build server \
 echo -e "\nStopping containers"
 docker stop dasz-api
 docker stop reverseproxy
+docker stop database
 
 echo -e "\nRemoving containers"
 docker rm dasz-api
 docker rm reverseproxy
+docker rm database
+
+echo -e "\nRunning database"
+docker run -d --name database \
+    -v $DIR/database/data:/var/lib/mysql \
+    -v $DIR/database/init:/docker-entrypoint-initdb.d \
+    -e MYSQL_ROOT_PASSWORD=password \
+    -e MYSQL_DATABASE=dasz-db \
+    mysql:5.7
 
 echo -e "\nRunning server"
 docker run -d --name dasz-api \
     -v $DIR/server:/usr/src/app \
+    --link database \
     dasz-api
 
 echo -e "\nRunning reverseproxy"
