fb485e4480ced4e1e044286ba1aa861123a1ecc2
jonatasvcosta <jonatas.atsoc@gmail.com>
2017-03-15 23:12:31 -0300

    Added news activity and got customtextview selected text

--- ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Components/CustomTextView.java
+++ ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Components/CustomTextView.java
@@ -2,8 +2,12 @@ package politcc2017.tcc_app.Components;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import politcc2017.tcc_app.Components.Helpers.DialogHelper;
 import politcc2017.tcc_app.Components.Helpers.FontHelper;
 
 /**
@@ -15,5 +19,31 @@ public class CustomTextView extends TextView {
     public CustomTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.setTypeface(FontHelper.get(FontHelper.TTF_FONT, getContext()));
+    }
+
+    public void allowWordContextMenu(){
+        setTextIsSelectable(true);
+        setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                DialogHelper.CustomDialog(getContext(), "Titulo", getText().subSequence(getSelectionStart(), getSelectionEnd()).toString(), "OK", "Cancelar").show();
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
     }
 }
