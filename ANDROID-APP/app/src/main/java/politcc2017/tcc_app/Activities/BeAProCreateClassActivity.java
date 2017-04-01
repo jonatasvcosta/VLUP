4331ceb2d27da4949b59dd996ed8d66087246392
jonatasvcosta <jonatas.atsoc@gmail.com>
2017-04-01 16:16:20 -0300

    Added editClass activity

---
+++ ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Activities/BeAProCreateClassActivity.java
@@ -0,0 +1,198 @@
+package politcc2017.tcc_app.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import io.github.mthli.knife.KnifeText;
+import politcc2017.tcc_app.Components.Helpers.FontHelper;
+import politcc2017.tcc_app.Components.Listeners.ContextMenuClickListener;
+import politcc2017.tcc_app.R;
+
+/**
+ * Created by Jonatas on 01/04/2017.
+ */
+
+public class BeAProCreateClassActivity extends BaseActivity implements ContextMenuClickListener{
+
+    private KnifeText classText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.be_a_pro_create_class);
+        classText = (KnifeText) findViewById(R.id.class_text);
+        classText.setTypeface(FontHelper.get(FontHelper.TTF_FONT, getApplicationContext()));
+        classText.setSelection(classText.getEditableText().length());
+        setupComponent();
+        setupEditorToolbarRightIcons(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        SaveText();
+        super.onBackPressed();
+    }
+
+    private void SaveText(){
+
+    }
+
+    private void setupComponent(){
+        setupBold();
+        setupItalic();
+        setupUnderline();
+        setupStrikethrough();
+        setupBullet();
+        setupQuote();
+        setupClear();
+    }
+
+    private void setupBold() {
+        ImageButton bold = (ImageButton) findViewById(R.id.bold);
+
+        bold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.bold(!classText.contains(KnifeText.FORMAT_BOLD));
+            }
+        });
+
+        bold.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_bold, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupItalic() {
+        ImageButton italic = (ImageButton) findViewById(R.id.italic);
+
+        italic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.italic(!classText.contains(KnifeText.FORMAT_ITALIC));
+            }
+        });
+
+        italic.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_italic, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupUnderline() {
+        ImageButton underline = (ImageButton) findViewById(R.id.underline);
+
+        underline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.underline(!classText.contains(KnifeText.FORMAT_UNDERLINED));
+            }
+        });
+
+        underline.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_underline, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupStrikethrough() {
+        ImageButton strikethrough = (ImageButton) findViewById(R.id.strikethrough);
+
+        strikethrough.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.strikethrough(!classText.contains(KnifeText.FORMAT_STRIKETHROUGH));
+            }
+        });
+
+        strikethrough.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_strikethrough, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupBullet() {
+        ImageButton bullet = (ImageButton) findViewById(R.id.bullet);
+
+        bullet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.bullet(!classText.contains(KnifeText.FORMAT_BULLET));
+            }
+        });
+
+
+        bullet.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_bullet, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupQuote() {
+        ImageButton quote = (ImageButton) findViewById(R.id.quote);
+
+        quote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.quote(!classText.contains(KnifeText.FORMAT_QUOTE));
+            }
+        });
+
+        quote.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_quote, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    private void setupClear() {
+        ImageButton clear = (ImageButton) findViewById(R.id.clear);
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                classText.clearFormats();
+            }
+        });
+
+        clear.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(BeAProCreateClassActivity.this, R.string.toast_format_clear, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v, String action) {
+        if(action.equals("undo")) classText.undo();
+        else if(action.equals("redo")) classText.redo();
+        else if(action.equals("save")) SaveText();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
