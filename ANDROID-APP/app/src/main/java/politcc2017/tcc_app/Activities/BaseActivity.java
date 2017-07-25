61c868b6196a056b38df29b692f2f3fc6c268b46
jonatasvcosta <jonatas.atsoc@gmail.com>
2017-07-24 23:28:32 -0300

    Added animation when scoring points

--- ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Activities/BaseActivity.java
+++ ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Activities/BaseActivity.java
@@ -18,8 +18,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -58,6 +63,8 @@ public class BaseActivity extends AppCompatActivity {
     private int appLanguage = 0;
     public int learningLanguage = 0;
     ImageView rightIcon, rightMostIcon, leftMostIcon, flagIcon;
+    private LinearLayout scorePointContainer;
+    private TextView scorePointText;
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
@@ -75,6 +82,8 @@ public class BaseActivity extends AppCompatActivity {
         rightMostIcon = (ImageView) findViewById(R.id.base_toolbar_rightmost_icon);
         leftMostIcon = (ImageView) findViewById(R.id.base_toolbar_leftmost_icon);
         flagIcon = (ImageView) findViewById(R.id.base_toolbar_language_icon);
+        scorePointContainer = (LinearLayout) findViewById(R.id.score_point_container);
+        scorePointText = (CustomTextView) findViewById(R.id.score_point_text);
         setSupportActionBar(toolbar);
         baseActionBar = getSupportActionBar();
         baseActionBar.setTitle("");
@@ -319,6 +328,29 @@ public class BaseActivity extends AppCompatActivity {
         Intent intent = getIntent();
         finish();
         startActivity(intent);
+    }
+
+    protected void scorePoints(String points){
+        scorePointText.setText(points);
+        fadeInAndOutImage(scorePointContainer);
+    }
+
+    private void fadeInAndOutImage(final View img)
+    {
+        img.setVisibility(View.VISIBLE);
+        final Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(750);
+        fadeOut.setAnimationListener(new Animation.AnimationListener()
+        {
+            public void onAnimationEnd(Animation animation){
+                img.setVisibility(View.GONE);
+            }
+            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationStart(Animation animation) {}
+        });
+
+        img.startAnimation(fadeOut);
     }
 }
 
--- ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Activities/HomeActivity.java
+++ ANDROID-APP/app/src/main/java/politcc2017/tcc_app/Activities/HomeActivity.java
@@ -65,7 +65,8 @@ public class HomeActivity extends BaseActivity {
         fadeout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fadeOutAndHideImage(flag);
+                scorePoints("+15");
+                fadeInAndOutImage(flag);
             }
         });
 
@@ -191,16 +192,15 @@ public class HomeActivity extends BaseActivity {
             setLanguage();
         }
     }
-    private void fadeOutAndHideImage(final ImageView img)
+    private void fadeInAndOutImage(final View img)
     {
-        Animation fadeOut = new AlphaAnimation(1, 0);
+        img.setVisibility(View.VISIBLE);
+        final Animation fadeOut = new AlphaAnimation(1, 0);
         fadeOut.setInterpolator(new AccelerateInterpolator());
-        fadeOut.setDuration(1000);
-
+        fadeOut.setDuration(500);
         fadeOut.setAnimationListener(new Animation.AnimationListener()
         {
-            public void onAnimationEnd(Animation animation)
-            {
+            public void onAnimationEnd(Animation animation){
                 img.setVisibility(View.GONE);
             }
             public void onAnimationRepeat(Animation animation) {}
