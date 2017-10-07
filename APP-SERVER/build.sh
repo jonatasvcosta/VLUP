f7c0764fbd25bfe2ceea574867aacbf43eaeb1e8
EC2 Default User <ec2-user@ip-172-31-22-225.ec2.internal>
2017-10-07 17:33:02 +0000

    Correção no build.sh de novo

--- APP-SERVER/build.sh
+++ APP-SERVER/build.sh
@@ -2,6 +2,7 @@ docker-compose down;
 eval `ssh-agent -s`
 ssh-add /home/ec2-user/.ssh/vlup_access_tcc
 git fetch;
+ssh-add /home/ec2-user/.ssh/vlup_access_tcc
 git pull origin master;
 docker-compose build;
 docker-compose up -d;
