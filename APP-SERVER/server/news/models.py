c6fa973ffa95a69e527d85037812b44b53be6317
Rogério Motisuki <rogerioyuuki@gmail.com>
2017-11-16 09:49:01 -0200

    Added description and image_url to Website

--- APP-SERVER/server/news/models.py
+++ APP-SERVER/server/news/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Website(models.Model):
     name = models.CharField(max_length=255)
     language = models.CharField(max_length=15, default="")
-    url = models.CharField(max_length=255)
+    description = models.CharField(max_length=300, default="")
+    image_url = models.URLField(max_length=500, default="")
+    url = models.URLField(max_length=500)
 
     def __str__(self):
     	return str(self.name)
